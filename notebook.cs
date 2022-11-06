4f8c9984973f701343435c2033cdb21a88e12432
ClearlyOllie <95652649+ClearlyOllie@users.noreply.github.com>
2022-11-06 19:14:56 +0000

    Add files via upload

---
+++ notebook.cs
@@ -0,0 +1,29 @@
+﻿string[] notes = new string[10];
+
+while (true)
+{
+
+    for (int i = 0; i < notes.Length; i++)
+    {
+        Console.WriteLine(i + ":" + notes[i]);
+    }
+
+    Console.Write("Enter note number: ");
+    int num = System.Convert.ToInt32(Console.ReadLine());
+    if (num <= 9 & num >= 0)
+    {
+        Console.Write("Enter note: ");
+        var note = Console.ReadLine();
+        if (note != null)
+        {
+            notes[num] = note.ToString();
+        }
+    }
+    else
+    {
+        Console.WriteLine("Enter a number between 0 and 9");
+    }
+    
+
+
+}
