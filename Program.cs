23f5819d3e1ca39cc21e93ac35d63ff30a186e7b
ClearlyOllie <95652649+ClearlyOllie@users.noreply.github.com>
2022-11-09 19:29:40 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,67 @@
-﻿class Program
+﻿class game
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        while (true)
-        {
-            Console.Write("How many runs? ");
-            int runs;
-            runs = Convert.ToInt32(Console.ReadLine());
+        List<string> tanks = new List<string>();
+        int[] choices = new int[8] { 0, 1, 2, 3, 4, 5, 6, 7 };
+        int shells = 30;
+        int score = 0;
 
-            Console.Write("How many numbers? ");
-            int nums;
-            nums = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < 10; i++)
+        { 
+            Random rand = new Random();
+            string tankx = Convert.ToString(rand.Next(0, 8));
+            string tanky = Convert.ToString(rand.Next(0, 8));
+            string tankPos = tankx + tanky;
 
-            for (int j = 1; j <= runs; j++)
+            if (tanks.Contains(tankPos))
             {
-                Console.WriteLine();
-                for (int i = 1; i <= nums; i++)
-                {
-                    Random rnd = new Random();
-                    int res = rnd.Next(1, 100);
-                    Console.Write($"{res,10}");
-                }
+                i -= 1;
             }
+            else
+            {
+                //Console.WriteLine(tankPos); //Writes the Position the tank is placed at
+                tanks.Add(tankPos);
+            }
+        }
+        
+        while (shells > 0 && tanks.Count > 0)
+        {
+            Console.Write("Enter X coordinate: "); ;
+            int xCor = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Y coordinate: ");
+            int yCor = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("\n");
+            if (choices.Contains(xCor) && choices.Contains(yCor))
+            {
+                foreach (string tank in tanks)
+                {
+                    int tank1 = int.Parse(tank[0].ToString());
+                    int tank2 = int.Parse(tank[1].ToString());
 
+                    if (xCor == int.Parse(tank[0].ToString()) && yCor == int.Parse(tank[1].ToString()))
+                    {
+                        score += 1;
+                        Console.WriteLine("That's a hit! Your score is {0}", score);
+                        tanks.Remove(tank);
+                        break;
+                    }
+                }
+                shells -= 1;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number between 0 & 7 (including 0 & 7)");
+            }
+  
+        }
+        if (tanks.Count == 0)
+        {
+            Console.WriteLine("Congratulations on hitting all 10 tanks!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time! Only {0} more tanks to go!", tanks.Count);
         }
     }
 }
